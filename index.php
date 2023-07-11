4874d8a3bd43b4f90bd2e7bf0400b1c506e7890d
Amjad  ALI SOOMRO <138880253+aptechamjad@users.noreply.github.com>
2023-07-11 16:33:08 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+
+// include ("nav.php"); 
+// include ("nav.php");  
+    //   include ("nav.php");  True
+
+    // include "nav.php";  true
+
+    // include "nav";
+    
+    //   require ("nav");
+//   require_once ("nav");
+//   require_once ("nav.php");
+ 
+    // include_once "nav.php";
+    // include_once "nav.php";
+
+    // include_once "nav.php";
+    ?>
+   <h1>body section </h1>
+</body>
+</html>
